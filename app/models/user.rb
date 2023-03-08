21fa25503c29134559571b30f89977fceb981c4a
oren roded <rn.roded@gmail.com>
2023-03-08 10:02:57 +0100

    finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true
+  validates :avatar_url, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
